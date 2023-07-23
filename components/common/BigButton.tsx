44f6f508107e6c6874572d0054ce147bc52be08e
Taylor Burdette <tsburdette@gmail.com>
2023-07-23 02:51:55 -0400

    Adds onClick to BigButton

--- components/common/BigButton.tsx
+++ components/common/BigButton.tsx
@@ -1,7 +1,10 @@
 const BigButton = (props: { text: string; onClick?: () => {} }) => {
     const { text, onClick } = props;
     return (
-        <button className="px-5 py-3 rounded-full bg-kaiba-red-500 hover:bg-kaiba-red-600 active:bg-kaiba-red-500">
+        <button
+            onClick={onClick}
+            className="px-5 py-3 rounded-full bg-kaiba-red-500 hover:bg-kaiba-red-600 active:bg-kaiba-red-500"
+        >
             {text}
         </button>
     );
