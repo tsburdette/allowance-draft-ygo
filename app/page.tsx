f408e899b53bb57226fbf8daa4ba3571e3515acb
Taylor Burdette <tsburdette@gmail.com>
2023-07-24 23:45:14 -0400

    Makes index page blank except header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import HeaderToolbar from "@/components/HeaderToolbar";
-import DeckBuilderPane from "@/components/DeckBuilderPane";
 
 export default function Home() {
     return (
         <main className="m-3 space-y-3">
             <HeaderToolbar />
-            <DeckBuilderPane />
         </main>
     );
 }
